d91321a941937d0d34044a0c56fd05de092fdab3
Montse1 <montse_codeer@outlook.com>
2022-05-12 18:45:43 -0500

    Modificacion 12-05-22

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,6 +36,7 @@
                     @endauth
                 </div>
             @endif
+            <h1>Servidores Practica</h1>
 
             <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                 <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
